a71883a770c7c2ac1693f72b696297a46d5aa078
lubomir1976 <lubomir1976@rambler.ru>
2020-12-25 09:08:38 +0400

    8 kyu 2 files

---
+++ remove-first-and-last-character.js
@@ -0,0 +1,10 @@
+function removeChar(str){
+    //You got this!
+   let string = str.split('');
+     string.shift('');
+     string.pop('');
+     return string.join('')
+   console.log(str.slice(0,-1));
+   };
+   
+   
