bcb621514bbac49b0a0b3622b117d1ddd0118c06
lubomir1976 <lubomir1976@rambler.ru>
2020-12-29 17:00:46 +0300

    Birthday I Cake

---
+++ Birthday-I-Cake.js
@@ -0,0 +1,9 @@
+function cake(x, y) {
+    let num = y
+      .split("")
+      .reduce(
+        (a, b, i) => a + (i % 2 === 0 ? b.charCodeAt(0) : b.charCodeAt(0) - 96),
+        0
+      );
+    return x * 0.7 > num ? "That was close!" : "Fire!";
+  }
